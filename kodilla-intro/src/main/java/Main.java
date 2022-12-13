ac8212d3018045701dc993489acee21ae961fdfd
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2022-12-13 11:22:16 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Jeremi";
+        int myAge = 25;
+        double height =1.83;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        String hello = "Hello from FirstClass!";
+        System.out.println(myAge);
+        System.out.println(hello);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String example1 = "abcd";
+        int example2 = 2137;
+        double example3 = 3.47;
+        char example4 = 'D';
+        boolean example5 = false;
+        String example6 = "efgh";
+        System.out.println(example1);
+        System.out.println(example2);
+        System.out.println(example3);
+        System.out.println(example4);
+        System.out.println(example5);
+        System.out.println(example6);
+
+    }
+}
