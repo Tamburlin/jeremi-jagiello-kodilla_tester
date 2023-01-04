08864e3c154450b91e71f59fe944f148e42acf0c
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2023-01-04 14:28:11 +0100

    m9

--- kodilla-intro/src/main/java/com/kodilla/RandomNumbers.java
+++ kodilla-intro/src/main/java/com/kodilla/RandomNumbers.java
@@ -1,2 +1,53 @@
-package com.kodilla;public class RandomNumbers {
+package com.kodilla;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        randMax();
+    }
+
+    public static void rand() {
+        int min = 0;
+        int max = 30;
+        int sum = 0;
+        int randomN;
+        int roof = 5000;
+        while (sum < roof) {
+            randomN = (int) Math.floor(Math.random() * (max - min + 1) + min);
+            sum = sum + randomN;
+        }
+    }
+
+    public static void randMin() {
+        int min = 0;
+        int max = 30;
+        int sum = 0;
+        int randomN;
+        int minimalHelper = 1;
+        int roof = 5000;
+        while (sum < roof) {
+            randomN = (int) Math.floor(Math.random() * (max - min + 1) + min);
+            sum = sum + randomN;
+            if (randomN < minimalHelper) {
+                minimalHelper = randomN;
+            }
+        }
+        System.out.println(minimalHelper);
+    }
+
+    public static void randMax() {
+        int min = 0;
+        int max = 30;
+        int sum = 0;
+        int randomN;
+        int maxHelper = 1;
+        int roof = 5000;
+        while (sum < roof) {
+            randomN = (int) Math.floor(Math.random() * (max - min + 1) + min);
+            sum = sum + randomN;
+            if (randomN > maxHelper) {
+                maxHelper = randomN;
+            }
+        }
+        System.out.println(maxHelper);
+    }
 }
