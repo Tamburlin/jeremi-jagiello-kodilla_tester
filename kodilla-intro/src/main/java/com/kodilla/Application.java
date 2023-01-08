3e0e0a7789dd95ada31ccc67880559b6bb55ecf8
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2023-01-08 17:06:27 +0100

    13

--- kodilla-intro/src/main/java/com/kodilla/Application.java
+++ kodilla-intro/src/main/java/com/kodilla/Application.java
@@ -1,22 +1,25 @@
 package com.kodilla;
-public class Application {
-        String name;
-        double age;
-        double height;
-        public Application(String name, double age, double height) {
-            this.name = name;
-            this.age = age;
-            this.height = height;
-        }
-    public static void main(String[] args) {
-           Application Mister1 = new Application("Adam", 40.5, 178);
-           if (Mister1.name != null) {
-               if (Mister1.age > 30 && Mister1.height >160) {
-                   System.out.println("User is older than 30 and taller than 160cm.");
-               } else {
-                   System.out.println("User is 30 (or younger) and/or 160 tall (or shorter).");
-               }
 
-           }
+public class Application {
+    String name;
+    double age;
+    double height;
+
+    public Application(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        Application Mister1 = new Application("Adam", 40.5, 178);
+        if (Mister1.name != null) {
+            if (Mister1.age > 30 && Mister1.height > 160) {
+                System.out.println("User is older than 30 and taller than 160cm.");
+            } else {
+                System.out.println("User is 30 (or younger) and/or 160 tall (or shorter).");
+            }
+
+        }
     }
 }
--- kodilla-intro/src/main/java/com/kodilla/Grades.java
+++ kodilla-intro/src/main/java/com/kodilla/Grades.java
@@ -1,47 +1,48 @@
 package com.kodilla;
 
-//public class Grades {
-//    private int[] grades;
-//    private int size;
-//
-//    public Grades() {
-//        this.grades = new int[10];
-//        this.size = 0;
-//    }
-//
-//    public void add(int value) {
-//        if (this.size == 10) {
-//            return;
-//        }
-//        this.grades[this.size] = value;
-//        this.size++;
-//    }
-//
-//    public int lastGrade(int size) {
-//        return this.grades[this.size - 1];
-//    }
-//    public static int averageGrades() {
-//        int average =0;
-//        for (int i = 0; i <= 0; i++) {
-//            average = this.grades[i] + average;
-//        }
-//        average = average / 10;
-//        return(average);
-//    }
-//
-//    public static void main(String[] args) {
-//        Grades studentA[];
-//        add(2);
-//        add(6);
-//        add(4);
-//        add(3);
-//        add(3);
-//        add(5);
-//        add(1);
-//        lastGrade(studentA.length);
-//        add(2);
-//        add(4);
-//        add(5);
-//        averageGrades();
-//    }
-//}
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+    public void add(int value) {
+        if (this.size ==10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+    public int lastGrade() {
+        System.out.println("Last added grade is: " + this.grades[this.size-1]);
+        return this.grades[this.size-1];
+    }
+    public double averageGrade() {
+        int average = 0;
+        for (int i = 0; i<10; i++){
+            average = average +this.grades[i];
+        }
+        average = average/this.size;
+        System.out.println(average);
+        return average;
+    }
+    public static void main(String[] args){
+        Grades grades = new Grades();
+        grades.add(1);
+        grades.add(5);
+        grades.add(3);
+        grades.add(2);
+        grades.add(1);
+        int x = grades.lastGrade();
+        System.out.println(x);
+        grades.add(6);
+        grades.add(4);
+        grades.add(1);
+        grades.add(6);
+        grades.add(4);
+        System.out.println("average grades = " +grades.averageGrade());
+    }
+}
+
