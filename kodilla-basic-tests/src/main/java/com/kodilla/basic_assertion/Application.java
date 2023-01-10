b76491bec0f0bf600cfee41a3efd117827ad46f9
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2023-01-10 17:50:21 +0100

    homework3.5 ver1

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -6,24 +6,24 @@ public class Application {
         Calculator calculator = new Calculator();
         int a = 5;
         int b = 8;
-        int sumResult = calculator.sum(a, b);
-        boolean correctSum = ResultChecker.assertEquals(13, sumResult);
+        double sumResult = calculator.sum(a, b);
+        boolean correctSum = ResultChecker.assertEquals(13, sumResult,0.00001);
         if (correctSum) {
             System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
         }else {
             System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
         }
 
-        int subtractResult = calculator.subtract(a, b);
-        boolean correctSubtract = ResultChecker.assertEquals(-3, subtractResult);
+        double subtractResult = calculator.subtract(a, b);
+        boolean correctSubtract = ResultChecker.assertEquals(-3, subtractResult, 0.0001);
         if (correctSubtract) {
             System.out.println("Metoda subtract  działa poprawnie dla liczb " + a + " i " + b);
         } else {
             System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
         }
 
-        int squareResult = calculator.square(a);
-        boolean correctSquare = ResultChecker.assertEquals(25, squareResult);
+        double squareResult = calculator.square(a);
+        boolean correctSquare = ResultChecker.assertEquals(25, squareResult, 0.0001);
         if (correctSquare) {
             System.out.println("Metoda square działa poprawnie  dla liczby " + a);
         } else {
--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,6 +1,6 @@
 package com.kodilla.basic_assertion;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,9 +9,15 @@ public class CalculatorTestSuite {
     @Test
     public void testSum() {
         Calculator calculator = new Calculator();
-        int a = 5;
-        int b = 8;
-        int sumResult = calculator.sum(a, b);
-        assertEquals(13,  sumResult);
+        double a = 5;
+        double b = 8;
+        double sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult, 0.0001);
+    }
+    @Test
+            public void  testSquare() {
+        Calculator calculator = new Calculator();
+        double squareResult = calculator.square(9);
+        assertEquals(81, squareResult, 0.001);
     }
 }
