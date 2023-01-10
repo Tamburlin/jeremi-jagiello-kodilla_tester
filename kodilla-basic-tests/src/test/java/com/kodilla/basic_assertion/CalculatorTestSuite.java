98af45f3122680f93c4bf40be098807bd38b7df1
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2023-01-10 17:51:49 +0100

    homework3.5 ver2

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -14,8 +14,9 @@ public class CalculatorTestSuite {
         double sumResult = calculator.sum(a, b);
         assertEquals(13, sumResult, 0.0001);
     }
+
     @Test
-            public void  testSquare() {
+    public void testSquare() {
         Calculator calculator = new Calculator();
         double squareResult = calculator.square(9);
         assertEquals(81, squareResult, 0.001);
