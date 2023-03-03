004da39cba95d7376442450a092bb298125f4fdb
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2023-03-03 11:57:05 +0100

    03.03ver2

--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -3,13 +3,14 @@ package com.kodilla.collections.lists.homework;
 import com.kodilla.collections.arrays.homework.CarUtils;
 import com.kodilla.collections.interfaces.homework.*;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class CarsListApplication {
 
     public static void main(String[] args) {
-        List<Car> cars = new LinkedList<>();
+        List<Car> cars = new ArrayList<>();
         cars.add(new Suzuki(1500));
         cars.add(new Opel(1400));
         cars.add(new Ford(1800));
@@ -21,8 +22,8 @@ public class CarsListApplication {
         }
 
         cars.remove(1);
-        Ford ford = new Ford(1800);
-        cars.remove(ford);
+        Ford autko = new Ford(1800);
+        cars.remove(autko);
         System.out.println(cars.size());
         for(Car car : cars) {
             CarUtils.describeCar(car);
