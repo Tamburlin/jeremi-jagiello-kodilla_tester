9913b84073a518f9ee65eeab4ba70420e433ca0f
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2023-03-04 19:37:03 +0100

    oldstuff2

--- kodilla-intro/src/main/java/com/kodilla/Book.java
+++ kodilla-intro/src/main/java/com/kodilla/Book.java
@@ -1,13 +1,18 @@
 package com.kodilla;
 
 public class Book {
-    private String author = "Isaac Asimov";
-    private String title = "Foundation";
-    public static Book of(){
-        return new Book();
+    private String author;
+    private String title;
+    public static Book of(String author, String title) {
+        return new Book(author, title);
     }
     public static void main(String[] args) {
-        Book book = Book.of();
+        Book book = Book.of("Issac Asimov", "The Foundation");
         System.out.println(book.author + " " + book.title);
+    }
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
     }
 }
