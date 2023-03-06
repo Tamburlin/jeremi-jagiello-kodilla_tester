c5ba246be74a4ad8fcdc3e050854d6749e8f51c4
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2023-03-06 11:08:34 +0100

    oldStuff04.03ver2

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,24 +1,46 @@
 package com.kodilla.basic_assertion;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CalculatorTestSuite {
+    private double a = 0;
+    private double b = 0;
+    private Calculator  calculator;
+    @BeforeAll
+    void initData() {
+        a = 9;
+        b = 8;
+    }
+    @BeforeEach
+    void initCalculator() {
+        calculator = new Calculator();
+    }
 
     @Test
     public void testSum() {
-        Calculator calculator = new Calculator();
-        double a = 5;
-        double b = 8;
         double sumResult = calculator.sum(a, b);
-        assertEquals(13, sumResult, 0.0001);
+        assertEquals(17, sumResult, 0.0001);
     }
 
     @Test
     public void testSquare() {
-        Calculator calculator = new Calculator();
-        double squareResult = calculator.square(9);
+        double squareResult = calculator.square(a);
         assertEquals(81, squareResult, 0.001);
+    }
+
+    @Test
+    public void testSubtract() {
+        double subtractResult = calculator.subtract(a, b);
+        assertEquals(1, subtractResult, 0.001);
     }
 }
