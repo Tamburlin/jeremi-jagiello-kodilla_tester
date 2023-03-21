57e96901632714c6745cfad03c59088f563c9a3f
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2023-03-21 15:40:21 +0100

    oldStuff

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametized_tests/homework/GamblingMachineTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametized_tests/homework/GamblingMachineTest.java
@@ -1,8 +1,31 @@
 package com.kodilla.parametized_tests.homework;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GamblingMachineTest {
-    GamblingMachine gamblingMachine1 = new GamblingMachine();
 
+    Set<Integer> numbers = new HashSet<>();
+    GamblingMachine gamblingMachine = new GamblingMachine();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/validGamblingMachineNumbers.csv", numLinesToSkip = 0, delimiter = ':')
+    public void shouldCorrectlyValidateNumbers(String stringNumbers) {
+        // given
+        String[] strings = stringNumbers.split(",");
+
+        for (String el : strings) {
+            numbers.add(Integer.valueOf(el));
+        }
+        // when
+        // then
+        assertDoesNotThrow(() -> gamblingMachine.howManyWins(numbers));
+    }
 }
